b80d7406ba24a3bc07471cf049e08a924fa5c87b
Valentin Fedulov <vasnake@gmail.com>
2024-02-16 16:04:07 +0200

    Monad Cond, lectures, done

--- chapter-3/test-monads.hs
+++ chapter-3/test-monads.hs
@@ -10,6 +10,7 @@
 {-# HLINT ignore "Using foldr on tuple" #-}
 {-# HLINT ignore "Using maximum on tuple" #-}
 {-# HLINT ignore "Use print" #-}
+{-# HLINT ignore "Use let" #-}
 
 module TestMonads where
 
@@ -38,6 +39,7 @@ import Data.Traversable (
     )
 
 import Control.Monad ( liftM, mplus, guard, mfilter, ap, guard, MonadPlus(..), when )
+-- import Control.Monad.Cont ( callCC )
 -- import Control.Monad (liftM, ap, MonadPlus(..), guard, msum)
 -- import Control.Applicative (Alternative(..))
 
@@ -48,7 +50,7 @@ import Prelude (
     (==), (*), id, const, Maybe(..), null, ($), succ, (.), undefined, Num(..), Show, Eq,
     foldr, foldl, Either(..), Monoid(..), Semigroup(..), putStrLn, print, (*), (>), (/), (^),
     map, (=<<), (>>=), return, flip, (++), fail, Ord(..), (>>), take, Monad(..),
-    Double, either
+    Double, either, Integer
     )
 
 newtype Except e a = Except { runExcept :: Either e a } deriving Show
@@ -160,3 +162,52 @@ sumSquares x y c =
     add x2 y2 $ \ss -> -- тут мы видим некотурую нелинейность
     c ss
 -- напоминает do-нотацию монады, не так ли?
+
+newtype Cont r a = Cont { runCont :: (a -> r) -> r }
+evalCont :: Cont r r -> r -- удобный враппер терминирования конт. на функции id (гарантирует совпадение типа а с типом эр)
+evalCont m = runCont m id
+
+instance Functor (Cont r) where
+    fmap = liftM
+
+instance Applicative (Cont r) where
+    pure = return
+    (<*>) = ap
+
+instance Monad (Cont r) where
+    return :: a -> Cont r a
+    return x = Cont (\c -> c x) -- монада над стрелочным типом, поэтому лямбда
+
+    (>>=) :: Cont r a -> (a -> Cont r b) -> Cont r b -- m bind k: монада бинд стрелкаКлейсли, на выходе монада
+    (Cont v) >>= k = Cont (\c -> v (\a -> runCont (k a) c)) -- лямбды, ибо работаем со стрелочным типом
+    -- нам должны дать конт. си, тогда мы запустим вычисление `v (...)`
+    -- в котором нам должны дать аргумент для стрелкаКлейсли
+
+sumIt :: Cont r Integer
+sumIt = do
+    a <- return 3
+    b <- return 5
+    return (a + b)
+
+test :: Integer -> Cont Integer Integer
+test x = do
+    a <- return 3
+    -- вместо `return ()` вмешаемся в вызов продолжения:
+    Cont (\c -> if x > 100 then 42 else c ()) -- выбор ветки исполнения следующего шага, в зависимости от аргумента
+    -- если х больше 100 то выходим, не вывываем продолжение, иначе вызываем `c ()`, где юнит это просто заглушка   
+    -- прерывание вычислений по условию
+    return (a + x)
+-- 42 портит нам полиморфизм, фиксируя параметр r в `Cont r a`
+-- callCC решает эту проблему
+
+test2 :: Integer -> Cont r Integer
+test2 x = callCC (\k -> do
+    a <- return 3
+    when (x > 100) (k 42) -- при выполнении условия здесь цепочка прерывается
+    -- иначе игнор строки и переход на следующую
+    return (a + x))
+-- k :: Integer -> Cont r b
+
+-- callCC :: ((a -> m b) -> m a) -> m a 
+callCC :: ((a -> Cont r b) -> Cont r a) -> Cont r a
+callCC f = Cont (\c -> runCont (f (\a -> Cont (\_ -> c a))) c)
